a2c32d50b572be53a73acbaede51b306aa86ab8b
Dandi Pratama <dandi.karunia@gmail.com>
2018-02-08 19:18:26 +0700

    add gulpfile.config

---
+++ gulpfile.config.js
@@ -0,0 +1,42 @@
+module.exports = {
+    dev: {
+        scss: './src/**/*.scss',
+        css: [
+            './node_modules/font-awesome/css/font-awesome.css',
+            './src/assets/css/**/*.css'
+        ],
+        js: [
+            './node_modules/angular/angular.min.js',
+            './node_modules/@uirouter/angularjs/release/angular-ui-router.js',
+            './src/**/*.module.js',
+            './src/**/*.routes.js',
+            './src/**/*.controller.js',
+            './src/**/*.component.js',
+            './src/**/*.service.js',
+            './src/assets/**/*.js'
+        ]
+    },
+    build: {
+        css: [
+            './node_modules/font-awesome/css/font-awesome.css',
+            './src/assets/css/**/*.css'
+        ],
+        js: [
+            './node_modules/angular/angular.min.js',
+            './node_modules/@uirouter/angularjs/release/angular-ui-router.js',
+            './src/**/*.module.js',
+            './src/**/*.routes.js',
+            './src/**/*.controller.js',
+            './src/**/*.component.js',
+            './src/**/*.service.js',
+            './src/assets/**/*.js'
+        ],
+        img: [
+            '.src/assets/img/*'
+        ],
+        html: [
+            './index.html',
+            './src/**/*.html'
+        ]
+    }
+}
