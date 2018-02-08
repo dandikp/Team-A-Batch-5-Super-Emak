6b2c040980d9a510529ebbd4141cbb1729980478
Dandi Pratama <dandi.karunia@gmail.com>
2018-02-08 21:13:17 +0700

    add supplerr style

---
+++ src/views/supplierAdd/supplier-add.component.js
@@ -0,0 +1,8 @@
+(function() {
+    angular.module
+        .component('supplierAdd', {
+            templateUrl: './src/views/supplierAdd/supplier-add.template.js',
+            controller: 'SupplierAddController',
+            controllerAs: 'vm'
+        })
+})
