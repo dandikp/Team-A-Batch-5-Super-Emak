a8161500b4558f6358e33b505498f3dff243eab5
Dandi Pratama <dandi.karunia@gmail.com>
2018-02-09 16:12:21 +0700

    init build configuration

--- gulpfile.config.js
+++ gulpfile.config.js
@@ -10,33 +10,17 @@ module.exports = {
             './node_modules/@uirouter/angularjs/release/angular-ui-router.js',
             './src/**/*.module.js',
             './src/**/*.routes.js',
+            './src/**/*.service.js',
             './src/**/*.controller.js',
             './src/**/*.component.js',
-            './src/**/*.service.js',
             './src/assets/**/*.js'
-        ]
+        ],
+        fonts: './node_modules/font-awesome/fonts/*'
     },
     build: {
-        css: [
-            './node_modules/font-awesome/css/font-awesome.css',
-            './src/assets/css/**/*.css'
-        ],
-        js: [
-            './node_modules/angular/angular.min.js',
-            './node_modules/@uirouter/angularjs/release/angular-ui-router.js',
-            './src/**/*.module.js',
-            './src/**/*.routes.js',
-            './src/**/*.controller.js',
-            './src/**/*.component.js',
-            './src/**/*.service.js',
-            './src/assets/**/*.js'
-        ],
-        img: [
-            '.src/assets/img/*'
-        ],
-        html: [
-            './index.html',
-            './src/**/*.html'
-        ]
+        css: './src/css/**/*.css',
+        js: './src/js/**/*.js',
+        img: '.src/img/*',
+        views: './src/**/*.html'
     }
 }
