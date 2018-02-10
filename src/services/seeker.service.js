f648e92ec553abaa0647fcb42594883c09a81644
Dandi Pratama <dandi.karunia@gmail.com>
2018-02-10 21:02:37 +0700

    init read and delete seeker

--- src/services/seeker.service.js
+++ src/services/seeker.service.js
@@ -1,34 +1,68 @@
 (function() {
-    angular.module('app')
-        .factory('seekerService', seekerService)
+	angular.module('app')
+		.factory('seekerService', seekerService)
 
-    seekerService.$inject = ['$window', '$http']
+	seekerService.$inject = ['$window', '$http']
 
-    function seekerService($window, $http) {
-        var service = {}
-        var baseUrl = 'https://mamabison-dev.heroku-app.com/api/v1/emaks'
-        var headers = {
-            'Access-Control-Allow-Origin': '*'
-        }
+	function seekerService($window, $http) {
+		var service = {}
+		var baseUrl = 'https://mamabison-dev.herokuapp.com/api/v1/emaks'
+		var headers = {
+			'Access-Control-Allow-Origin': '*'
+		}
 
-        service.seeker = []
-        service.getSeekersPerPage = getSeekersPerPage    
-        
-        return service
+		service.seeker = []
+		service.getSeekersPerPage = getSeekersPerPage   
+		service.deleteSeeker = deleteSeeker 
+		
+		return service
 
-        function getSeekersPerPage(page, limitDataPerPage, onSuccess, onError) {
-            $http({
-                method: 'GET',
-                url: baseUrl + '?page=' + page + '&limit=' + limitDataPerPage,
-                headers: headers,
-                data: ''
-            })
-                .then(function(response) {
-                    onSuccess(response.data)
-                    console.log(response)
-                }, function(response) {
-                    onError(response.statusText)
-                })
-        }
-    }
-})()
+		function getSeekersPerPage(page, limitDataPerPage, onSuccess, onError) {
+			$http({
+				method: 'GET',
+				url: baseUrl + '?page=' + page + '&limit=' + limitDataPerPage,
+				headers: headers,
+				data: ''
+			})
+				.then(function(response) {
+					onSuccess(response.data)
+				}, function(response) {
+					onError('Couldn\'t Fetch Data')
+				})
+		}
+
+		function addSeeker(payload, onSuccess, onError) {
+			$http({
+				method: 'POST',
+				url: baseUrl,
+				headers: headers,
+				data: {
+					name: payload.name,
+					username: payload.username,
+					password: payload.password,
+					phone: payload.phone,
+					email: payload.email,
+					address: payload.address
+				}
+			})
+				.then(function(response) {
+					console.log(response)
+				}, function(response) {
+					console.log(response)
+				})
+		}
+
+		function deleteSeeker(id, onSuccess, onError) {
+			$http({
+				method: 'DELETE',
+				url: baseUrl + '/' + id,
+				headers: headers
+			})
+				.then(function(response) {
+					onSuccess(response.status)
+				}, function (response) {
+					onError(response.status)
+				})
+		}
+	}
+})();
--- src/views/seeker/seeker.controller.js
+++ src/views/seeker/seeker.controller.js
@@ -1,40 +1,53 @@
 (function() {
-    angular.module('app')
-        .controller('SeekerController', SeekerController)
-    
-    SeekerController.$inject = ['$window', 'seekerService']
+	angular.module('app')
+		.controller('SeekerController', SeekerController)
+	
+	SeekerController.$inject = ['$window', 'seekerService']
 
-    function SeekerController($window, seekerService) {
-        var vm = this
-        vm.seeker = []
-        vm.dataFetched = false
-        vm.dataPerPage = 10
-        vm.totalPage = -1
-        vm.currentPage = null
+	function SeekerController($window, seekerService) {
+		var vm = this
+		vm.seeker = []
+		vm.dataFetched = false
+		vm.dataPerPage = 10
+		vm.totalPage = -1
+		vm.currentPage = null
 
-        vm.init = init
-        vm.getSeekerPerPage = getSeekerPerPage
+		vm.init = init
+		vm.getSeekersPerPage = getSeekersPerPage
+		vm.deleteSeeker = deleteSeeker
 
-        function init() {
-            vm.getSeekerPerPage(1)
-        }
+		function init() {
+			vm.getSeekersPerPage(1)
+		}
 
-        function getSeekerPerPage(page) {
-            if (vm.currentPage === page) return
-            if (page === 0) return
-            if (page === vm.totalPage.length + 1) return
+		function getSeekersPerPage(page) {
+			if (vm.currentPage === page) return
+			if (page === 0) return
+			if (page === vm.totalPage.length + 1) return
 
-            vm.dataFetched = false
-            vm.currentPage = page
+			vm.dataFetched = false
+			vm.currentPage = page
 
-            seekerService.getSeekerPerPage(page, vm.dataPerPage, function(data) {
-                vm.seeker = data.data
-                vm.totalPage = new Array(data.total_page)
-                vm.dataFetched = true
+			seekerService.getSeekersPerPage(page, vm.dataPerPage, function(data) {
+				vm.seeker = data.data
+				vm.totalPage = new Array(data.total_page)
+				vm.dataFetched = true
+			}, function(error) {
+				$window.alert(error)
+			})
+		}
 
-            }, function(error) {
-                $window.alert(error)
-            })
-        }
-    }
-})()
+		function deleteSeeker(id) {
+			vm.dataFetched = false
+
+			seekerService.deleteSeeker(id, function(response) {
+				$window.alert(response)
+					.then(function() {
+						vm.dataFetched = true
+					})
+			}, function(error) {
+				$window.alert(error)
+			})
+		}
+	}
+})();
