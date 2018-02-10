14ecd4409383dfc9b8d87cd75a6ccff812c3fe6a
Irham Nur Hakim <irhamhqm@gmail.com>
2018-02-10 21:19:25 +0700

    testing

--- src/components/sideNav/sideNav.component.js
+++
@@ -1,9 +0,0 @@
-(function () {
-  angular
-    .module('app')
-    .component('sideNav', {
-      templateUrl: 'src/components/sideNav/sideNav.template.html',
-      controller: 'sideNavCtrl',
-      controllerAs: 'vm'
-    })
-})();
--- src/components/sideNav/sideNav.controller.js
+++
@@ -1,9 +0,0 @@
-(function () {
-  angular
-    .module('app')
-    .controller('sideNavCtrl', controller)
-
-  function controller() {
-
-  }
-})();
